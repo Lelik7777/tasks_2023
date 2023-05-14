e1856fca6ff161ed3616a5177f281b99c5cc1878
Lelik Jan <21alex7777@gmail.com>
2023-05-14 19:31:02 +0300

    feat: add info about scope

--- functions/functions.js
+++ functions/functions.js
@@ -1,1 +1,11 @@
 console.log("hello form function");
+//не станет свойством объекта window
+const globalConst='global constants';
+const getGlobalArrow=()=>{};
+
+//станут свойствами объекта window
+var globalValue = "global";
+var globalFun=()=>{}
+function getGlobalValueCustom(params) {
+
+}
