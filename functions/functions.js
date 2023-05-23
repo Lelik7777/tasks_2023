34b4e9e93d9590a4a58522f67c71a9351301a3f2
Lelik Jan <21alex7777@gmail.com>
2023-05-23 09:40:34 +0300

    feat: add info and tasks about this

--- functions/functions.js
+++ functions/functions.js
@@ -889,3 +889,45 @@ let user = {
 };
 
 setTimeout(user.sayHi, 1000); // Привет, undefined!
+//1. вариант решения: сделать обертку
+setTimeout(()=>{user.sayHi()}, 1000);
+//2. second variant - using bind()
+setTimeout(user.sayHi.bind(user),1000);
+
+//
+function askPassword(ok, fail) {
+  let password = prompt("Password?", '');
+  if (password == "rockstar") ok();
+  else fail();
+}
+
+let user1 = {
+  name: 'Вася',
+
+  loginOk() {
+    alert(`${this.name} logged in`);
+  },
+
+  loginFail() {
+    alert(`${this.name} failed to log in`);
+  },
+
+};
+
+//askPassword(user1.loginOk.bind(user1), user1.loginFail.bind(user1));
+
+function askPassword1(ok, fail) {
+  let password = prompt("Password?", '');
+  if (password == "rockstar") ok();
+  else fail();
+}
+
+let user2 = {
+  name: 'John',
+
+  login(result) {
+    alert( this.name + (result ? ' logged in' : ' failed to log in') );
+  }
+};
+
+askPassword(user2.login.bind(user2,true),user2.login.bind(user2,false)); // ?
