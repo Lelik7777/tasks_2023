963537e16137f5c3b20fa3423e327b6cf4c58e8c
Lelik Jan <21alex7777@gmail.com>
2023-07-24 18:44:39 +0300

    feat:add index.html and matrix.js

--- slider/slider.js
+++ slider/slider.js
@@ -8,7 +8,7 @@ const createCardTemplate = () => {
   const card = document.createElement("div");
   card.classList.add("card");
   return card;
-};
+}
 
 const moveLeft = () => {
   CAROUSEL.classList.add("transition-left");
@@ -46,46 +46,4 @@ CAROUSEL.addEventListener("animationend", (animationEvent) => {
 
   BTN_LEFT.addEventListener("click", moveLeft);
   BTN_RIGHT.addEventListener("click", moveRight);
-});
-
-const card = document.querySelector(".card");
-let computedStyle = getComputedStyle(card);
-console.log(computedStyle.width);
-
-//? implementation slider via opacity
-const images = document.querySelectorAll(".images img");
-let i = 0;
-const bntPrev = document.querySelector(".images__buttons .prev");
-const bntNext = document.querySelector(".images__buttons .next");
-
-bntNext.addEventListener("click", function () {
-  images[i].className = "";
-  i++;
-  if (i >= images.length - 1) {
-    i = 0;
-  }
-  images[i].className = "showed";
-  setDisabledButton(this, bntPrev);
-});
-bntPrev.addEventListener("click", function () {
-  images[i].className = "";
-  i--;
-  if (i < 0) {
-    i = images.length - 1;
-  }
-  images[i].className = "showed";
-  setDisabledButton(this, bntNext);
-});
-let myTimeout;
-const setDisabledButton = (btn1, btn2) => {
-  btn1.setAttribute("disabled", true);
-  btn2.setAttribute("disabled", true);
-  myTimeout = setTimeout(() => {
-    btn1.removeAttribute("disabled");
-    btn2.removeAttribute("disabled");
-  }, 1000);
-};
-window.onbeforeunload = function (params) {
-
-  clearTimeout(myTimeout);
-};
+})
