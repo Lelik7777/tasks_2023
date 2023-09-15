ba79f9875e12c3e3f64960b878933baedacc1808
Lelik Jan <21alex7777@gmail.com>
2023-09-15 10:39:34 +0300

    feat: add info in draft.js

--- draft/draft.js
+++ draft/draft.js
@@ -377,3 +377,54 @@ const snakeByPass = (arr) => {
 
 };
 snakeByPass(matrix);
+
+const text=`Lorem ipsum dolor sit amet consectetur adipisicing elit.
+<br>
+ Corporis necessitatibus aspernatur nulla a enim? Quisquam minima iusto,
+<br>
+ consequatur accusamus vero deserunt ut, ducimus possimus,<br> explicabo temporibus dolorum sapiente.
+
+ Neque, ullam`
+
+//  const block=document.querySelector('.block');
+//  document.body.insertAdjacentHTML('afterbegin',text)
+
+// const circle = {
+//   radius: 10,
+//   style: {
+//       color: 'blue'
+//   }
+// };
+
+// const clonedCircle = {
+//   ...circle
+// };
+// const copy=circle;
+// //copy.radius=33;
+// clonedCircle.radius=20;
+// console.log(circle);
+
+class Circle {
+  constructor(radius) {
+      this.radius = radius;
+  }
+  set diameter(value) {
+      this.radius = value / 2;
+      console.log('SET ', value);
+  }
+  get diameter() {
+      return this.radius * 2;
+  }
+}
+
+let circle = new Circle(100);
+
+let cloneCircle1 = Object.assign(circle, {
+  diameter: 200
+});
+
+let cloneCircle2 = {
+  ...circle
+};
+cloneCircle2.diameter=200;
+console.log(cloneCircle2);
