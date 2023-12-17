2c8aed005368d9e779c895064b734a59704554e6
Lelik Jan <21alex7777@gmail.com>
2023-12-17 06:03:19 +0300

    feat: add logic in bst.js

--- binary search tree/bst.js
+++ binary search tree/bst.js
@@ -30,11 +30,98 @@ class BinarySearchTree {
       return node;
     }
   }
-  has(value) {}
-  remove(value) {}
-  min() {}
-  max() {}
-  leftTraverse() {}
+  has(value) {
+    return searchWithin(this.root, value);
+
+    function searchWithin(node, value) {
+      if (!node) {
+        return false;
+      }
+      if (value === node.value) {
+        return true;
+      }
+      return value < node.value ? searchWithin(node.left, value) : searchWithin(node.right, value);
+    }
+  }
+  remove(value) {
+    this.root = removeNode(this.root, value);
+
+    function removeNode(node, value) {
+      if (!node) {
+        return null;
+      }
+      if (value < node.value) {
+        node.left = removeNode(node.left, value);
+        return node;
+      } else if (value > node.value) {
+        node.right = removeNode(node.right, value);
+        return node;
+      } else {
+        if (!node.left && !node.right) {
+          return null;
+        }
+        if (!node.left) {
+          node = node.right;
+          return node;
+        }
+
+        if (!node.right) {
+          node = node.left;
+          return node;
+        }
+        let minRightNode = node.right;
+        while (minRightNode.left) {
+          minRightNode = minRightNode.left;
+        }
+        node.value = minRightNode.value;
+        node.right = removeNode(node.right, minRightNode.value);
+
+        return node;
+      }
+    }
+  }
+  min() {
+    if (!this.root) {
+      return;
+    }
+    let node = this.root;
+    while (node.left) {
+      node = node.left;
+    }
+    return node.value;
+  }
+  max() {
+    if (!this.root) {
+      return;
+    }
+    let node = this.root;
+    while (node.right) {
+      node = node.right;
+    }
+    return node.value;
+  }
+  leftTraverse(cb) {
+    doLeftTraverse(this.root, cb);
+
+    function doLeftTraverse(node, cb) {
+      if (node) {
+        doLeftTraverse(node.left, cb);
+        cb(node.value);
+        doLeftTraverse(node.right, cb);
+      }
+    }
+  }
+  rightTraverse(cb) {
+    doRightTraverse(this.root, cb);
+
+    function doRightTraverse(node, cb) {
+      if (node) {
+        doRightTraverse(node.right, cb);
+        cb(node.value);
+        doRightTraverse(node.left, cb);
+      }
+    }
+  }
 }
 
 const bst = new BinarySearchTree();
@@ -45,10 +132,11 @@ bst.add(20);
 bst.add(18);
 bst.add(32);
 bst.add(25);
-//bst.remove(19);
+//bst.remove(18);
 console.log(bst);
 console.log(bst.has(44));
-console.log(bst.has(19));
+console.log(bst.has(15));
 console.log("min value", bst.min());
 console.log("max value", bst.max());
 console.log(bst.leftTraverse((val) => console.log(val)));
+console.log(bst.rightTraverse((val) => console.log(val)));
